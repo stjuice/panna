4643eaa0f3dbee5283571a19428436653aa2b93f
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2026-03-05 22:10:30 +0200

    Nice rows

--- src/components/orders/OrderTable.tsx
+++ src/components/orders/OrderTable.tsx
@@ -19,7 +19,7 @@ const formatDate = (date?: string) => {
 	const dd = String(d.getDate()).padStart(2, "0");
 	const mm = String(d.getMonth() + 1).padStart(2, "0");
 	const yyyy = d.getFullYear();
-	return `${dd}/${mm}/${yyyy}`;
+	return `${dd}.${mm}.${yyyy}`;
 };
 
 const OrderTable = ({ orders }: OrderTableProps) => {
