272e477891a9fc5bd0cb67a4d96b2cf4bfd2dcaa
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2026-03-07 11:17:23 +0200

    Run anumation once

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import styles from "./home.module.scss";
 import MainButton from "@/components/buttons/MainButton";
 import SecondaryButton from "@/components/buttons/SecondaryButton";
 
 const HomePage = () => {
+	const [animate, setAnimate] = useState(false);
+
+	useEffect(() => {
+		const alreadyPlayed = sessionStorage.getItem("home_animation_played");
+
+		if (!alreadyPlayed) {
+			setAnimate(true);
+			sessionStorage.setItem("home_animation_played", "true");
+		}
+	}, []);
+
 	return (
 		<div className={styles.container}>
-			<img src="/logo_for_white.png" alt="Panna wedding salon" className={styles.logo} />
-			<div className={styles.buttons}>
+			<img
+				src="/logo_for_white.png"
+				alt="Panna wedding salon"
+				className={`${styles.logo} ${!animate ? styles.logoStatic : ""}`}
+			/>
+
+			<div
+				className={`${styles.buttons} ${!animate ? styles.buttonsStatic : ""}`}
+			>
 				<MainButton to="/search">Знайти</MainButton>
-				<SecondaryButton to="/orders/new">Створити замовлення</SecondaryButton>
+				<SecondaryButton to="/orders/new">
+					Створити замовлення
+				</SecondaryButton>
 			</div>
 		</div>
 	);
