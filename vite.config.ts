4103cc26bda80d339549ff19c90ff472eb6cd4f2
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2026-03-07 20:04:25 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Panna Wedding Salon",
+				short_name: "Panna",
+				start_url: "/",
+				display: "standalone",
+				background_color: "#ffffff",
+				theme_color: "#7794b6",
+				orientation: "portrait",
+				icons: [
+					{
+						src: "/icons/icon-192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/icons/icon-512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
